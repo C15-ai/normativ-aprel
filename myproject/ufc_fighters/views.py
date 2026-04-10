bc7c620a6996f6af4649994ccb4dd978157080b5
momin <tursunov545@gmai.com>
2026-04-10 18:41:23 +0500

    4-normativ

--- myproject/ufc_fighters/views.py
+++ myproject/ufc_fighters/views.py
@@ -1,16 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
 from .models import Fighter
 from .forms import FighterForm
-
+from django.db.models import Q
+from django.core.paginator import Paginator
 def fighter_list(request):
+    search = request.GET.get('search', '')
+    page_number = request.GET.get('page')
+    print(page_number)
+    # print(search)
     fighter = Fighter.objects.all()
-    return render(request, 'fighter/fighter_list.html',{'fighter':fighter})
+    if search:
+        fighter = fighter.filter(Q(name__icontains=search) | Q(nickname__icontains=search) | Q(weight_class__icontains=search))
+    paginator = Paginator(fighter,3)
 
-# def fighter_create_form(request):
-#     form = FighterForm()
-#     return render(request,'fighter/create_fighters',{'form':form})
+    fighter = paginator.get_page(page_number)
+
+    return render(request, 'fighter/fighter_list.html',{'fighter':fighter , 'search' : search})
+
+
+
 def fighter_create(request):
     if request.method == "POST":
         form = FighterForm(request.POST)
@@ -23,7 +32,7 @@ def fighter_create(request):
 def fighter_update_form(request,pk=None):
     fighter = Fighter.objects.filter(id=pk).first()
     form = FighterForm(instance=fighter)
-    return render(request, 'fighter/update_fighters.html',{'form':form, 'fighter':fighter})
+    return render(request, 'fighter/update_fighters.html',{'form':form, 'fighter':fighter , })
 
 def fighter_update(request,pk=None):
     fighter = Fighter.objects.filter(id=pk).first()
